2c0b06f70fa399f4f6143a4100df7949ccc68e48
ericking97 <ericking97@hotmail.com>
2018-09-09 10:04:19 -0500

    Add table resultados

---
+++ migrations/20180909142849-create-table-resultados.js
@@ -0,0 +1,79 @@
+/* jshint node: true */
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('resultados', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        field: 'id_resultados'
+      },
+      idPyme: {
+        type: Sequelize.INTEGER,
+        field: 'id_pyme',
+        references: {
+          model: 'pyme',
+          key: 'id_pyme'
+        }
+      },
+      saldoTasaCrecimiento: {
+        type: Sequelize.FLOAT,
+        field: 'saldo_tasa_crecimiento'
+      },
+      saldoIncrementoTotal: {
+        type: Sequelize.FLOAT,
+        field: 'saldo_incremento_total'
+      },
+      depositoTasaCrecimiento: {
+        type: Sequelize.FLOAT,
+        field: 'deposito_tasa_crecimiento'
+      },
+      depositoIncrementoTotal: {
+        type: Sequelize.FLOAT,
+        field: 'deposito_incremento_total'
+      },
+      deudaTotal: {
+        type: Sequelize.FLOAT,
+        field: 'deuda_total'
+      },
+      porcentajePagado: {
+        type: Sequelize.FLOAT,
+        field: 'porcentaje_pagado'
+      },
+      creditosSolicitados: {
+        type: Sequelize.INTEGER,
+        field: 'creditos_solicitados'
+      },
+      creditosExternos: {
+        type: Sequelize.FLOAT,
+        field: 'creditos_externos'
+      },
+      inversionExterna: {
+        type: Sequelize.FLOAT,
+        field: 'inversion_externa'
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+        allowNull: false,
+        field: 'created_at'
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+        allowNull: false,
+        field: 'updated_at'
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        field: 'deleted_at'
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('resultados');
+  }
+};
---
+++ models/Resultados.js
@@ -0,0 +1,67 @@
+/* jshint node: true */
+'use strict';
+
+const debug = require('debug')('estimacion:models:Resultados');
+
+module.exports = (sequelize, DataTypes) => {
+
+    const Resultados = sequelize.define('Resultados', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            field: 'id_resultados'
+        },
+        idPyme: {
+            type: DataTypes.INTEGER,
+            field: 'id_pyme',
+            references: {
+                model: 'pyme',
+                key: 'id_pyme'
+            }
+        },
+        saldoTasaCrecimiento: {
+            type: DataTypes.FLOAT,
+            field: 'saldo_tasa_crecimiento'
+        },
+        saldoIncrementoTotal: {
+            type: DataTypes.FLOAT,
+            field: 'saldo_incremento_total'
+        },
+        depositoTasaCrecimiento: {
+            type: DataTypes.FLOAT,
+            field: 'deposito_tasa_crecimiento'
+        },
+        depositoIncrementoTotal: {
+            type: DataTypes.FLOAT,
+            field: 'deposito_incremento_total'
+        },
+        deudaTotal: {
+            type: DataTypes.FLOAT,
+            field: 'deuda_total'
+        },
+        porcentajePagado: {
+            type: DataTypes.FLOAT,
+            field: 'porcentaje_pagado'
+        },
+        creditosSolicitados: {
+            type: DataTypes.INTEGER,
+            field: 'creditos_solicitados'
+        },
+        creditosExternos: {
+            type: DataTypes.FLOAT,
+            field: 'creditos_externos'
+        },
+        inversionExterna: {
+            type: DataTypes.FLOAT,
+            field: 'inversion_externa'
+        }
+    }, {
+        tableName: 'resultados',
+        underscored: true,
+        paranoid: true
+    });
+
+    return Resultados;
+
+};
